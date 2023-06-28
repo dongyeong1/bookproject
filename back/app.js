1036af8fd6279d4e0f10e325f741f2706e3c21ec
dongyeong1 <rjsdud0227@naver.com>
2023-06-28 20:25:35 +0900

    back

--- back/app.js
+++ back/app.js
@@ -1,52 +1,52 @@
-const express=require('express')
-const postRouter=require('./routes/post')
-const userRouter=require('./routes/user')
-const db=require('./models')
-const cors=require('cors')
-const passportConfig=require('./passport')
-const passport = require('passport')
+const express = require("express");
+const postRouter = require("./routes/post");
+const userRouter = require("./routes/user");
+const db = require("./models");
+const cors = require("cors");
+const passportConfig = require("./passport");
+const passport = require("passport");
 const session = require("express-session");
-const cookieParser=require('cookie-parser')
-const dotenv=require('dotenv')
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
 
+dotenv.config();
 
-dotenv.config()
+const app = express();
 
-const app=express();
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  }));
-app.use(express.json());//axios로 데이터보냃때
-app.use(express.urlencoded({ extended: true }));//일반폼을보낼때
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+);
+app.use(express.json()); //axios로 데이터보냃때
+app.use(express.urlencoded({ extended: true })); //일반폼을보낼때
 
-db.sequelize.sync()
-.then(()=>{
-    console.log('db연결성공')
-})
+db.sequelize.sync().then(() => {
+    console.log("db연결성공");
+});
 
 passportConfig();
 
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
-  app.use(cookieParser(process.env.COOKIE_SECRET));
-
-
-
-app.use(session({
-    saveUninitialized: false,
-    resave: false,
-    secret:process.env.COOKIE_SECRET
-}));
+app.use(
+    session({
+        saveUninitialized: false,
+        resave: false,
+        secret: process.env.COOKIE_SECRET,
+    })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get("/", (req, res) => {
+    res.send("hello");
+});
 
+app.use("/post", postRouter);
+app.use("/user", userRouter);
 
-app.use('/post',postRouter)
-app.use('/user',userRouter)
-
-
-
-app.listen(3065,()=>{
-    console.log('서버실행중')
-})
+app.listen(3065, () => {
+    console.log("서버실행중");
+});
