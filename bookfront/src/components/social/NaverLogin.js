74e317e0c1754a8b3c233a9c922754e76b2ec331
dongyeong1 <rjsdud0227@naver.com>
2023-06-29 21:26:50 +0900

    naver

--- bookfront/src/components/social/NaverLogin.js
+++ bookfront/src/components/social/NaverLogin.js
@@ -7,7 +7,7 @@ const NaverLogin = () => {
         "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" +
         client_id +
         "&redirect_uri=" +
-        encodeURI("http://localhost:3000/NaverOauth") +
+        encodeURI("http://13.209.99.236/NaverOauth") +
         "&state=" +
         Math.random().toString(36).substr(3, 14);
 
